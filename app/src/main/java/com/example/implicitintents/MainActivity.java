eacb1721beae5e943334b8ab4231d9f63bb08621
408091 <a408091@2PP2141100003.local>
2023-01-25 16:38:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.implicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText mShareTextEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.et_url);
+        mLocationEditText = findViewById(R.id.et_location);
+        mShareTextEditText = findViewById(R.id.et_text);
+    }
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        Intent chooser = Intent.createChooser(intent, "TEST TITLE"); // 앱 선택기 강제 적용
+        startActivity(chooser);
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        startActivity(intent);
+    }
+
+    public void shareText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder // 공유를 위한 앱 선택자
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Share this text with: ")
+                .setText(txt)
+                .startChooser();
+    }
+
+    public void goNext(View view) {
+        startActivity(new Intent(this, ReceiverActivity.class));
+    }
+
+    public void goCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        startActivity(intent);
+    }
+}
